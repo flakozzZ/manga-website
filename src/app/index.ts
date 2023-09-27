5b966a7b02641179ef49bbf4f9d79eed4e2aa731
Arsen <76997450+Esinaliev@users.noreply.github.com>
2023-09-27 22:06:10 +0600

    second

--- src/app/index.ts
+++
@@ -1,8 +0,0 @@
-import {createApp} from "vue";
-import {createPinia} from './providers/store.ts'
-import router from "./providers/router.ts";
-import App from "./index.vue";
-import "./index.scss"
-
-
-export const app = createApp(App).use(createPinia).use(router)
--- src/main.ts
+++
@@ -1,3 +0,0 @@
-import {app} from "./app/index.ts";
-
-app.mount('#app')
