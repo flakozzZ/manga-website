c53212d47e7c2d5dcbe5673e192d6e9f43a2a83a
Abay Kossayev <a.kosaev@inbox.ru>
2023-10-01 15:49:08 +0600

    commited

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 import svgLoader from 'vite-svg-loader'
+import { fileURLToPath, URL } from 'node:url'
+
+
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   plugins: [vue(), svgLoader()],
