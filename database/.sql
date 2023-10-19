ac60d6fa3facb39310cd0ae1b05de8470dae4cec
Abay <a.kosaev@inbox.ru>
2023-10-19 12:41:06 +0600

    back

--- database/.sql
+++ database/.sql
@@ -73,3 +73,17 @@ SELECT books.title
 	JOIN public.books on public.book_tags.book_id = public.books.book_id
 	WHERE public.book_tags.tag_id = (SELECT tag_id FROM tags WHERE name = 'tag2')
 	GROUP BY public.books.title;
+
+
+SELECT books.title
+    FROM book_genres
+    JOIN books on book_genres.genre_id = books.book_id
+    WHERE book_genres.genre_id = (SELECT genre_id FROM genres WHERE name = 'yuri')
+
+SELECT public.books.title, public.book_comments.text, public.users.username as name
+	FROM public.team_members
+	JOIN public.users ON public.team_members.user_id = public.users.user_id
+	WHERE public.teams.team_id = (SELECT teams.team_id
+								 FROM teams
+								 WHERE title = 'Oshi no ko')
+	ORDER BY books.title
