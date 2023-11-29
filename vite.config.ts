1b6dcfb63c470ed0a973c909bd23f0c9829c500b
Abay <a.kosaev@inbox.ru>
2023-11-30 01:03:16 +0600

    bugs resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
-import { fileURLToPath } from 'url'; // Use 'url' module from Node.js
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
-      },
-    ]
-  }
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [vue(), svgLoader()],
 });
