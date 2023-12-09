4fc86e7d11eb5c42434c603bfdf0ea61f158cd42
Abay <a.kosaev@inbox.ru>
2023-12-09 16:15:41 +0600

    fix

--- src/shared/ui/side-panel/index.vue
+++ src/shared/ui/side-panel/index.vue
@@ -8,12 +8,12 @@ export default {
 <template>
   <div
       v-if="props.show"
-      class="h-auto fixed top-0 left-0 w-full bg-black/70 z-40"
+      class="min-h-screen fixed top-0 left-0 w-full bg-black/70 z-40"
       @click="$emit('close')"
   >
   </div>
   <div :class="props.show ? 'translate-x-0' : '-translate-x-full' "
-       class="fixed side-panel top-0 left-0 z-40 w-64 h-auto p-4 overflow-y-auto transition-transform">
+       class="fixed side-panel top-0 left-0 z-40 w-64 min-h-screen p-4 overflow-y-auto transition-transform">
     <div class="pt-4 pr-2.5">
       <Icon @click="$emit('close')" class="cursor-pointer absolute right-3 mt-1" name="close"/>
       <slot></slot>
